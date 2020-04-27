a1cc222d591034d09608e5a5b9559e65959e0686
2016519 <1352924502@qq.com>
2020-04-27 14:14:04 +0800

    Initial commit

---
+++ owntracks/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class OwntracksConfig(AppConfig):
+    name = 'owntracks'
+    verbose_name = u"Log日志"
+
---
+++ servermanager/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ServermanagerConfig(AppConfig):
+    name = 'servermanager'
+    verbose_name = u"服务器"
